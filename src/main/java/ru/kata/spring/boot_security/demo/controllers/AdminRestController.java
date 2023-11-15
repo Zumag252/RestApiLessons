0688aa052b63c6bc750474e55bc6cc2d3be7a706
zumag <artem2522462@mail.ru>
2023-11-15 15:54:54 +0900

    commit admin1

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminRestController.java
@@ -1,7 +1,5 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
