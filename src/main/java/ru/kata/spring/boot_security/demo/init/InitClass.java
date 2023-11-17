2000ad7fd424e459e6821c9ce2418b5e7d7c9262
zumag <artem2522462@mail.ru>
2023-11-17 12:51:54 +0900

    commit 6

--- src/main/java/ru/kata/spring/boot_security/demo/init/InitClass.java
+++ src/main/java/ru/kata/spring/boot_security/demo/init/InitClass.java
@@ -21,14 +21,16 @@ public class InitClass {
     @PostConstruct
     @Transactional
     public void postConstruct() {
-        var roleAdmin = Set.of(new Role("ROLE_ADMIN"));
-        var roleUser = Set.of(new Role("ROLE_USER"));
+        Set<Role> roleAdmin = Set.of(new Role("ROLE_ADMIN"));
+        Set<Role> roleUser = Set.of(new Role("ROLE_USER"));
 
-        User admin = new User("admin", "admin",
+        User admin = new User("admin", "adminov",
                 44, "admin@mail.ru", "admin", roleAdmin);
-        User user = new User("user", "user",
+        User user = new User("user", "userov",
                 35, "user@mail.ru", "user", roleUser);
+        User user1 = new User("max","zhuravlev", 35,"max@mail.ru","max", roleUser);
         userService.saveUser(admin);
         userService.saveUser(user);
+        userService.saveUser(user1);
     }
 }
