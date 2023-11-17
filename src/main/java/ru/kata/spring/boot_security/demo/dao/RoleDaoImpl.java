1c5b2839c54965e937f8f9c9f8a90d6117509365
zumag <artem2522462@mail.ru>
2023-11-17 13:15:34 +0900

    commit 10

--- src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDaoImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDaoImpl.java
@@ -3,7 +3,6 @@ package ru.kata.spring.boot_security.demo.dao;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.Role;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Set;
--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
@@ -1,17 +1,13 @@
 package ru.kata.spring.boot_security.demo.dao;
 
 import ru.kata.spring.boot_security.demo.model.User;
-
 import java.util.List;
 
 
 public interface UserDao {
     List<User> findAll();
-
     void save(User user);
-
     User findById(Long id);
     User findByEmail(String username);
-
     void deleteById(Long id);
 }
